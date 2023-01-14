f9f2a7c75b2b3121f10e622cc5396f9f7926944a
Ashishbindapi <ashish.k@bindapi.com>
2023-01-14 16:06:52 +0530

    git

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,6 +1,5 @@
 export default {
     template : `
-    <router-link to="/"></router-link>
         <router-view></router-view>
     `,
 }
--- assets/js/components/page.js
+++ assets/js/components/page.js
@@ -1,3 +1,44 @@
 export default {
-    template : `<h1>Ashish kumar</h1>`
+    template : `
+    <select @change="loadStates($event)"  id="countries">
+        <option value="">Select country</option>
+        <option v-for="country in countries" :value="country.id" :data-key="country.id">{{country.country_name}}</option>
+    </select>
+
+    <select id="states" @change="loadDistric($event)" v-show="stateVisibility">
+        <option value="">Select states</option>
+        <option v-for="state in states" :value="state.state_id" :data-key="state.state_id">{{state.state_name}}</option>
+    </select>
+    <select id="distric" v-show="districVisibility">
+        <option value="">Select distric</option>
+        <option v-for="distric in district" :value="distric.district_id" :data-key="distric.district_id">{{distric.district_name}}</option>
+    </select>`,
+    data(){
+        return{
+            countries : [],
+            states : [],
+            district : [],
+            stateVisibility: false,
+            districVisibility:false
+        }
+    },
+    mounted : function(){
+        axios
+            .get('http://dev7.pro/gitSelect/api/countries.php')
+            .then(response => (this.countries = response.data))
+    },
+    methods : {
+        loadStates: function($event) {
+            axios
+                .get('http://dev7.pro/gitSelect/api/states.php?country_id='+$event.target.value)
+                .then(response => (this.states = response.data)),
+            this.stateVisibility = true
+        },
+        loadDistric: function($event) {
+            axios
+                .get('http://dev7.pro/gitSelect/api/district.php?steate_id='+$event.target.value)
+                .then(response => (this.district = response.data)),
+            this.districVisibility = true
+        }
+    }
 }
